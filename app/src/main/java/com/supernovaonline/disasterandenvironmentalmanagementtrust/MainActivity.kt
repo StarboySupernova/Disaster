7fc740f80241aa7e28b49df58ea0b2eb10060104
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-03-12 09:04:21 +0100

    First Component Unexpected behaviour when rendering images found to be due to bug in Android Studio Electric Eel 2022.1.1 Patch 1 (https://issuetracker.google.com/issues/206862224?pli=1)

--- app/src/main/java/com/supernovaonline/disasterandenvironmentalmanagementtrust/MainActivity.kt
+++ app/src/main/java/com/supernovaonline/disasterandenvironmentalmanagementtrust/MainActivity.kt
@@ -3,13 +3,25 @@ package com.supernovaonline.disasterandenvironmentalmanagementtrust
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.supernovaonline.disasterandenvironmentalmanagementtrust.ui.theme.DisasterAndEnvironmentalManagementTrustTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Text("Hello Jetpack Compose")
                 }
             }
         }
@@ -30,14 +42,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun OnBoardingCard() {
+    Card {
+        Image(
+            painter = painterResource(id = R.drawable.cardblur),
+            contentDescription = "Card background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(27.dp)
+        ) {
+            Text(
+                "Disaster and Environmental Management Trust",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                "Promoting environmental preservation strategies, awareness and sustainability actions at a grassroots level",
+                color = Color.White.copy(0.8f),
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.padding(bottom = 27.dp))
+            Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(percent = 50), modifier = Modifier.border(
+                width = 1.dp,
+                color = Color.White.copy(0.5f),
+                shape = RoundedCornerShape(percent = 50)
+            )) {
+                Text(
+                    "Get Started Now",
+                    modifier = Modifier.padding(horizontal = 40.dp, vertical = 4.dp),
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun CardPreview () {
     DisasterAndEnvironmentalManagementTrustTheme {
-        Greeting("Android")
+        OnBoardingCard()
     }
 }
+
