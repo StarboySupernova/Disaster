d41fa32e99aa5c24a31caa954ce17edde3087b48
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-03-16 00:27:00 +0100

    imported full Material Icons in gradle module file

--- app/src/main/java/com/supernovaonline/disasterandenvironmentalmanagementtrust/RootScreen.kt
+++ app/src/main/java/com/supernovaonline/disasterandenvironmentalmanagementtrust/RootScreen.kt
@@ -3,8 +3,11 @@ package com.supernovaonline.disasterandenvironmentalmanagementtrust
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.supernovaonline.disasterandenvironmentalmanagementtrust.home.HomeScreen
 
 @Composable
@@ -18,3 +21,13 @@ fun RootScreen() {
         }
     }
 }
+
+sealed class NavigationItem(var route: String, var icon: ImageVector, var title: String) {
+    object Login : NavigationItem("login", Icons.Filled.Home, "Home")
+    object Home : NavigationItem("home", Icons.Filled.Home, "Home")
+    object Stats : NavigationItem("stats", Icons.Filled.Analytics, "Stats")
+    object Add : NavigationItem("add", Icons.Filled.Add, "Add")
+    object Search : NavigationItem("search", Icons.Filled.Search, "Search")
+    object Profile : NavigationItem("profile", Icons.Filled.Person, "Profile")
+}
+
