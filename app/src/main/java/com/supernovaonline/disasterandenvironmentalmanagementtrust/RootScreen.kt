8ce64fbdd8ad6ed106d4f0a1c250e31e44a23200
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-03-16 01:32:22 +0100

    Bottom App Bar implemented, albeit with no actions defined yet

--- app/src/main/java/com/supernovaonline/disasterandenvironmentalmanagementtrust/RootScreen.kt
+++ app/src/main/java/com/supernovaonline/disasterandenvironmentalmanagementtrust/RootScreen.kt
@@ -1,18 +1,29 @@
 package com.supernovaonline.disasterandenvironmentalmanagementtrust
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import com.supernovaonline.disasterandenvironmentalmanagementtrust.home.HomeScreen
 
 @Composable
 fun RootScreen() {
-    Scaffold() { padding ->
+    Scaffold(
+        bottomBar = {
+            BottomBar()
+        }
+    ) { padding ->
         Column(
             modifier = Modifier
                 .padding(padding)
@@ -31,3 +42,34 @@ sealed class NavigationItem(var route: String, var icon: ImageVector, var title:
     object Profile : NavigationItem("profile", Icons.Filled.Person, "Profile")
 }
 
+@Composable
+fun BottomBar() {
+    val items = listOf(
+        NavigationItem.Home,
+        NavigationItem.Stats,
+        NavigationItem.Add,
+        NavigationItem.Search,
+        NavigationItem.Profile
+    )
+
+    BottomNavigation(
+        backgroundColor = Color(33, 17, 52),
+        contentColor = Color.White
+    ) {
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Image(
+                    imageVector = item.icon,
+                    contentDescription = item.title,
+                    modifier = Modifier.size(30.dp),
+                    colorFilter = ColorFilter.tint(Color.White)
+                )},
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.White.copy(0.4f),
+                alwaysShowLabel = false,
+                selected = false,
+                onClick = { /* TODO */ }
+            )
+        }
+    }
+}
