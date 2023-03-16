7008c5c71fa545eefe6a1a7ed5096ba66aae4570
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-03-16 02:15:33 +0100

    Xreated RankingRow & RankingTable, currently fed dummy data from rankings property

---
+++ app/src/main/java/com/supernovaonline/disasterandenvironmentalmanagementtrust/stats/RankingTable.kt
@@ -0,0 +1,39 @@
+package com.supernovaonline.disasterandenvironmentalmanagementtrust.stats
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.supernovaonline.disasterandenvironmentalmanagementtrust.Ranking
+import com.supernovaonline.disasterandenvironmentalmanagementtrust.rankings
+import com.supernovaonline.disasterandenvironmentalmanagementtrust.ui.theme.DisasterAndEnvironmentalManagementTrustTheme
+
+@Composable
+fun RankingTable(rankings: List<Ranking>) {
+    LazyColumn(
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 20.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        itemsIndexed(rankings) { index, ranking ->
+            RankingRow(
+                index = index,
+                title = ranking.title,
+                image = ranking.image,
+                change = ranking.percentChange,
+                eth = ranking.eth
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RankingTablePreview() {
+    DisasterAndEnvironmentalManagementTrustTheme {
+        RankingTable(rankings = rankings)
+    }
+}
