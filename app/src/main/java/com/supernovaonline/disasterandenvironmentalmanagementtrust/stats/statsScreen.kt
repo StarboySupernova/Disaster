8d091436773607cf0fd7e7ff9619092cd1042cdf
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-03-16 10:19:03 +0100

    created custom tab component ins statsScreen.kt

--- app/src/main/java/com/supernovaonline/disasterandenvironmentalmanagementtrust/stats/statsScreen.kt
+++ app/src/main/java/com/supernovaonline/disasterandenvironmentalmanagementtrust/stats/statsScreen.kt
@@ -1,16 +1,19 @@
 package com.supernovaonline.disasterandenvironmentalmanagementtrust.stats
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Assessment
+import androidx.compose.material.icons.filled.TrackChanges
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -46,6 +49,7 @@ fun StatsScreen() {
         Column(
             Modifier.padding(bottom = 30.dp)
         ) {
+            CustomTabComponent()
             Card(
                 backgroundColor = Color.White.copy(0.15f),
                 elevation = 0.dp,
@@ -57,6 +61,70 @@ fun StatsScreen() {
             }
         }
     }
+}
+
+@Composable
+fun CustomTabComponent() {
+    var tabIndex by remember { mutableStateOf(0) }
+    val tabData = listOf(
+        "Ranking" to Icons.Default.Assessment,
+        "Activity" to Icons.Default.TrackChanges
+    )
+    TabRow(
+        selectedTabIndex = tabIndex,
+        backgroundColor = Color.Transparent,
+        contentColor = Color.White,
+        divider = {
+            TabRowDefaults.Divider(
+                thickness = 1.dp,
+                color = Color(66, 34, 104)
+            )
+        },
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                modifier = Modifier.customTabOffset(tabPositions[tabIndex]),
+                height = 4.dp,
+                color = Color(148, 103, 255)
+            )
+        }
+    ) {
+        tabData.forEachIndexed { index, pair ->
+            Tab(
+                selected = tabIndex == index,
+                onClick = {
+                    tabIndex = index
+                },
+                content = {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        modifier = Modifier.padding(bottom = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(imageVector = pair.second, contentDescription = null)
+                        Text(
+                            pair.first,
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 17.sp,
+                            color = Color.White
+                        )
+                    }
+                }
+            )
+        }
+    }
+}
+
+fun Modifier.customTabOffset(tabPosition: TabPosition): Modifier = composed {
+    val indicatorWidth = 118.dp
+    val currentTabWidth = tabPosition.width
+    val indicatorOffset by animateDpAsState(
+        targetValue = tabPosition.left + currentTabWidth / 2 - indicatorWidth / 2,
+        animationSpec = tween(durationMillis = 1000, easing = FastOutSlowInEasing)
+    )
+    fillMaxWidth()
+        .wrapContentSize(Alignment.BottomStart)
+        .offset(x = indicatorOffset)
+        .width(indicatorWidth)
 }
 
 @Preview
